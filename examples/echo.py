85fc2062c0ef78b43b7d2200efe1963847a74001
Roman Novatorov <roman.novatorov@gmail.com>
2019-05-12 21:07:22 +0300

    Add examples

---
+++ examples/echo.py
@@ -0,0 +1,48 @@
+import os
+
+import trio
+import triogram
+
+
+def new_message(update):
+    return 'message' in update
+
+
+async def echo_once(bot):
+    """
+    Waits for a new message and sends the received text back exactly once.
+    """
+    update = await bot.wait(new_message)
+    await bot.api.sendMessage(params={
+        'chat_id': update['message']['from']['id'],
+        'text': update['message']['text']
+    })
+
+
+async def echo(bot):
+    """
+    Waits for new messages and sends the received text back.
+    """
+    async with bot.sub(new_message) as updates:
+        async for update in updates:
+            await bot.api.sendMessage(params={
+                'chat_id': update['message']['from']['id'],
+                'text': update['message']['text']
+            })
+
+
+async def main():
+    """
+    Starts the bot and event handlers.
+    """
+    token = os.environ['TOKEN']
+    bot = triogram.make_bot(token)
+
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(bot)
+        nursery.start_soon(echo_once, bot)
+        nursery.start_soon(echo, bot)
+
+
+if __name__ == '__main__':
+    trio.run(main)
