655542ed8fb8706163bd6d1be14666e500b42bba
Roman Novatorov <roman.novatorov@gmail.com>
2019-05-12 19:52:13 +0300

    Adapt api.API

--- src/triogram/api.py
+++ src/triogram/api.py
@@ -8,12 +8,12 @@ class Api:
         self._throttler = throttler
         self._request_wrapper = request_wrapper
 
-    async def __call__(self, method_name, **params):
+    async def __call__(self, method_name, **kwargs):
         async def make_request():
             async with self._throttler():
-                return await self._session.get(
+                return await self._session.post(
                     path=f'/{method_name}',
-                    params={**self._meta_params, **params}
+                    **kwargs
                 )
 
         if self._request_wrapper is not None:
@@ -22,41 +22,13 @@ class Api:
         response = await make_request()
         payload = response.json()
 
-        try:
-            return payload['response']
-        except KeyError:
-            raise ApiError(payload['error']) from None
+        if payload['ok']:
+            return payload['result']
 
-    def __getattr__(self, key):
-        return MethodGroup(name=key, api=self)
+        raise ApiError(payload['description'])
 
-    @property
-    def _meta_params(self):
-        return {
-            'access_token': self._access_token,
-            'v': self._version,
-        }
+    def __getattr__(self, method_name):
+        async def method(**kwargs):
+            return await self(method_name, **kwargs)
 
-
-class MethodGroup:
-
-    def __init__(self, name, api):
-        self.name = name
-        self.api = api
-
-    def __getattr__(self, key):
-        return Method(name=key, group=self)
-
-
-class Method:
-
-    def __init__(self, name, group):
-        self.name = name
-        self.group = group
-
-    @property
-    def full_name(self):
-        return f'{self.group.name}.{self.name}'
-
-    async def __call__(self, **params):
-        return await self.group.api(self.full_name, **params)
+        return method
