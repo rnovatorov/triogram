16a0bcd1bd0c05f447951bf614c5f75abb321b78
Roman Novatorov <roman.novatorov@gmail.com>
2019-05-14 01:28:42 +0300

    Extend tests

---
+++ tests/test_factories.py
@@ -0,0 +1,7 @@
+from triogram.factories import make_bot
+
+
+async def test_make_bot():
+    token = '123:ABC'
+    bot = make_bot(token)
+    assert bot.api._session.endpoint == f'/bot{token}'
---
+++ tests/test_poller.py
@@ -0,0 +1,32 @@
+from triogram.poller import Poller
+
+
+class MockApi:
+
+    def __init__(self, updates):
+        self._updates = iter(updates)
+
+    async def get_updates(self, *_, **__):
+        return next(self._updates)
+
+
+async def test_sanity():
+    api = MockApi(updates=[
+        [{'update_id': 0, 'message': 'A'}],
+        [{'update_id': 1, 'message': 'B'}, {'update_id': 2, 'message': 'B'}],
+        [{'update_id': 3, 'message': 'C'}],
+    ])
+    poller = Poller(api=api)
+
+    async with poller() as updates:
+        assert (await updates.__anext__())['message'] == 'A'
+        assert poller._offset is None
+
+        assert (await updates.__anext__())['message'] == 'B'
+        assert poller._offset == 1
+
+        assert (await updates.__anext__())['message'] == 'B'
+        assert poller._offset == 2
+
+        assert (await updates.__anext__())['message'] == 'C'
+        assert poller._offset == 3
