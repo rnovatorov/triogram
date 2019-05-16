45a2e1cddc23b3fb11392eacbc2107996e7fe9f2
Roman Novatorov <roman.novatorov@gmail.com>
2019-05-16 23:22:32 +0300

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='triogram',
-    version='0.0.1',
+    version='0.0.2',
     description='Async Telegram Bot API built with asks and trio',
     packages=find_packages('src'),
     package_dir={'': 'src'},
