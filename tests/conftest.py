6e42e92be6f01568b12a6ac229e39f5c6ac32d3d
Roman Novatorov <roman.novatorov@gmail.com>
2019-08-01 23:42:14 +0300

    Rewrite tests using `http.server`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,58 +1,54 @@
+import json
 import functools
+from http import server
+from urllib import parse
 
 import trio
 import pytest
-import flask
-from werkzeug import serving
 
 from triogram.api import Api
 from triogram.session import make_session
 
 
-server_ready = trio.Event()
+class Server(server.HTTPServer):
+    def __init__(self, *args, **kwargs):
+        self.ready = trio.Event()
+        super().__init__(*args, **kwargs)
 
-
-class Handler(serving.WSGIRequestHandler):
-
-    protocol_version = "HTTP/1.1"
-
-
-class Server(serving.BaseWSGIServer):
     def server_activate(self):
         super().server_activate()
-        server_ready.set()
+        self.ready.set()
+
+
+class Handler(server.BaseHTTPRequestHandler):
+    def do_POST(self):
+        url = parse.urlparse(self.path)
+        params = dict(parse.parse_qsl(url.query))
+        method = url.path.split("/")[-1]
+        ok = "error" not in params
+        content = json.dumps(
+            {
+                "ok": ok,
+                "result" if ok else "description": {"method": method, "params": params},
+            }
+        )
+        self.send_response(server.HTTPStatus.OK)
+        self.send_header("Content-Length", len(content))
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Connection", "close")
+        self.end_headers()
+        self.wfile.write(content.encode())
 
 
 @pytest.fixture(name="api")
 async def fixture_api(nursery):
-    app = flask.Flask(__name__)
-    token = "123:ABC"
-
-    @app.route(f"/bot{token}/<name>", methods=["POST"])
-    def method(name):
-        params = flask.request.args
-        ok = "_error" not in params
-
-        return flask.jsonify(
-            {
-                "ok": ok,
-                "result"
-                if ok
-                else "description": {"method_name": name, "params": params},
-            }
-        )
-
-    server = Server(host="localhost", port=0, app=app, handler=Handler)
-
+    httpd = Server(("", 0), Handler)
     nursery.start_soon(
         functools.partial(
-            trio.run_sync_in_worker_thread, server.serve_forever, cancellable=True
+            trio.to_thread.run_sync, httpd.serve_forever, cancellable=True
         )
     )
-    await server_ready.wait()
-
-    base_location = f"http://{server.host}:{server.port}"
-    session = make_session(token, base_location=base_location)
-    api = Api(session=session)
-
-    yield api
+    await httpd.ready.wait()
+    host, port = httpd.server_address
+    session = make_session("123:ABC", f"http://{host}:{port}")
+    yield Api(session)
--- tests/test_api.py
+++ tests/test_api.py
@@ -6,16 +6,16 @@ from triogram.errors import ApiError
 async def test_api(api):
     # Response OK
     response = await api.get_chat_member(params={"chat_id": "@foo", "user_id": 42})
-    assert response["method_name"] == "getchatmember"
+    assert response["method"] == "getchatmember"
     assert response["params"]["chat_id"] == "@foo"
     assert response["params"]["user_id"] == "42"
 
     # Response error
     try:
-        await api.send_message(params={"_error": True, "chat_id": 43, "text": "bar"})
+        await api.send_message(params={"error": True, "chat_id": 43, "text": "bar"})
     except ApiError as exc:
         response = exc.args[0]
-        assert response["method_name"] == "sendmessage"
+        assert response["method"] == "sendmessage"
         assert response["params"]["chat_id"] == "43"
         assert response["params"]["text"] == "bar"
     else:
