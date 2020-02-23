532686ecc569f01eda387583cc69c52074eaa425
Roman Novatorov <roman.novatorov@gmail.com>
2020-02-23 15:03:32 +0300

    http: add workaround for httpx#817

--- src/triogram/http.py
+++ src/triogram/http.py
@@ -1,9 +1,23 @@
+import logging
+
 import httpx
 
 from .config import TELEGRAM_API_URL, HTTP_TIMEOUT
 
 
+logger = logging.getLogger(__name__)
+
+
+class RetryingClient(httpx.AsyncClient):
+    async def send(self, *args, **kwargs):
+        try:
+            return await super().send(*args, **kwargs)
+        except KeyError:
+            logger.exception("See: https://github.com/encode/httpx/issues/817")
+            return await super().send(*args, **kwargs)
+
+
 def http_client(token, telegram_api_url=TELEGRAM_API_URL, http_timeout=HTTP_TIMEOUT):
     base_url = f"{telegram_api_url}/bot{token}/"
     timeout = httpx.Timeout(http_timeout)
-    return httpx.AsyncClient(base_url=base_url, timeout=timeout)
+    return RetryingClient(base_url=base_url, timeout=timeout)
