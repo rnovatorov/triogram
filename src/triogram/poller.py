289a3125a1ce4761db1660fbb0580ccfa244b40f
Roman Novatorov <roman.novatorov@gmail.com>
2020-03-02 01:04:35 +0300

    poller: retry get updates

--- src/triogram/poller.py
+++ src/triogram/poller.py
@@ -1,6 +1,14 @@
-import attr
+import logging
 
-from .config import POLLER_TIMEOUT
+import attr
+import httpx
+import trio
+
+from .config import POLLER_RETRY_INTERVAL, POLLER_TIMEOUT
+from .errors import ApiError, AuthError
+
+
+logger = logging.getLogger(__name__)
 
 
 @attr.s
@@ -9,8 +17,25 @@ class Poller:
     _api = attr.ib()
     _offset = attr.ib(default=None)
     _timeout = attr.ib(default=POLLER_TIMEOUT)
+    _retry_interval = attr.ib(default=POLLER_RETRY_INTERVAL)
 
     async def get_updates(self):
+        while True:
+            try:
+                return await self._get_updates()
+            except AuthError:
+                raise
+            except ApiError as exc:
+                logger.error(
+                    "get updates error, will retry after %d: %s",
+                    self._retry_interval,
+                    exc,
+                )
+                await trio.sleep(self._retry_interval)
+            except httpx.TimeoutException as exc:
+                logger.warning("get updates timeout, will retry asap: %s", exc)
+
+    async def _get_updates(self):
         updates = await self._api.get_updates(
             params={"offset": self._offset, "timeout": self._timeout}
         )
