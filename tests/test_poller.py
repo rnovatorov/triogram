cf810b41a1e94fd93a1f0206f89b2df43c48aba2
Roman Novatorov <roman.novatorov@gmail.com>
2020-03-03 00:35:55 +0300

    poller: test error handling

--- tests/test_poller.py
+++ tests/test_poller.py
@@ -1,5 +1,9 @@
 import attr
+import httpx
+import pytest
+import trio
 
+from triogram.errors import ApiError, AuthError
 from triogram.poller import Poller
 
 
@@ -9,7 +13,70 @@ class MockApi:
     _updates = attr.ib()
 
     async def get_updates(self, *_, **__):
-        return next(self._updates)
+        u = next(self._updates)
+        if isinstance(u, Exception):
+            raise u
+        return u
+
+
+async def test_auth_error():
+    api = MockApi(iter([AuthError()]))
+    poller = Poller(api=api)
+
+    with pytest.raises(AuthError):
+        await poller.get_updates()
+
+
+async def test_api_error(autojump_clock):
+    api = MockApi(
+        iter(
+            [
+                [{"update_id": 0, "message": "A"}],
+                ApiError(),
+                [{"update_id": 1, "message": "B"}],
+            ]
+        )
+    )
+    poller = Poller(api=api, retry_interval=2)
+
+    updates = await poller.get_updates()
+    assert len(updates) == 1
+    assert updates[0]["message"] == "A"
+    assert poller._offset == 1
+
+    start_time = trio.current_time()
+
+    updates = await poller.get_updates()
+    assert len(updates) == 1
+    assert updates[0]["message"] == "B"
+    assert poller._offset == 2
+
+    end_time = trio.current_time()
+
+    assert 1 < end_time - start_time < 3
+
+
+async def test_timeout():
+    api = MockApi(
+        iter(
+            [
+                [{"update_id": 0, "message": "A"}],
+                httpx.TimeoutException(),
+                [{"update_id": 1, "message": "B"}],
+            ]
+        )
+    )
+    poller = Poller(api=api)
+
+    updates = await poller.get_updates()
+    assert len(updates) == 1
+    assert updates[0]["message"] == "A"
+    assert poller._offset == 1
+
+    updates = await poller.get_updates()
+    assert len(updates) == 1
+    assert updates[0]["message"] == "B"
+    assert poller._offset == 2
 
 
 async def test_sanity():
