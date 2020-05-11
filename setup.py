32fbc2fb640a89e48e925665978131c329d7b125
Roman Novatorov <roman.novatorov@gmail.com>
2020-05-11 18:39:02 +0300

    setup: import package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+import setuptools
 
 
-setup(
+setuptools.setup(
     name="triogram",
     version="0.3.2",
     description="Async Telegram Bot API built with httpx and trio",
-    packages=find_packages("src"),
+    packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
     url="https://github.com/rnovatorov/triogram",
     author="Roman Novatorov",
